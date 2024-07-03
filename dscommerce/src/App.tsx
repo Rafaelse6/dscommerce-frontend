aee263a73231967b834c715bd3b6a18d135f99da
Rafael Santos <rafa.05125@gmail.com>
2024-07-02 23:15:24 -0300

    Adding React Router DOM

--- dscommerce/src/App.tsx
+++ dscommerce/src/App.tsx
@@ -1,6 +1,19 @@
-import Catalog from "./routes/Catalog";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ClientHome from "./routes/ClientHome";
+import Catalog from "./routes/ClientHome/Catalog";
+import ProductDetails from "./routes/ClientHome/ProductDetails";
 
 function App() {
-  return <Catalog />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ClientHome />}>
+          <Route index element={<Catalog />} />
+          <Route path="catalog" element={<Catalog />} />
+          <Route path="product-details" element={<ProductDetails />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 export default App;
