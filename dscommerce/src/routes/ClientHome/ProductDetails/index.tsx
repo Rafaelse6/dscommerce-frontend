e425faa8f47a3cde739c654efb4530a953df7db2
Rafael Santos <rafa.05125@gmail.com>
2024-07-06 23:51:23 -0300

    First request with Axios

--- dscommerce/src/routes/ClientHome/ProductDetails/index.tsx
+++ dscommerce/src/routes/ClientHome/ProductDetails/index.tsx
@@ -5,11 +5,21 @@ import ProductDetailsCard from "../../../components/ProductDetailsCard";
 import * as productService from "../../../services/product-service.ts";
 import "./styles.css";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { ProductDTO } from "../../../models/product.ts";
+import axios from "axios";
 
 export default function ProductDetails() {
   const params = useParams();
 
-  const product = productService.findById(Number(params.productId));
+  const [product, setProduct] = useState<ProductDTO>();
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/products/1").then((response) => {
+      console.log(response.data);
+      setProduct(response.data);
+    });
+  }, []);
 
   return (
     <main>
