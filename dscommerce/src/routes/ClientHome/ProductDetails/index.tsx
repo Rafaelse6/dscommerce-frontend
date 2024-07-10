78bd67920595b4aba490e16ed5fbd9a63af2a377
Rafael Santos <rafa.05125@gmail.com>
2024-07-10 01:17:51 -0300

    Catch request error

--- dscommerce/src/routes/ClientHome/ProductDetails/index.tsx
+++ dscommerce/src/routes/ClientHome/ProductDetails/index.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
@@ -11,13 +11,19 @@ import { ProductDTO } from "../../../models/product.ts";
 export default function ProductDetails() {
   const params = useParams();
 
+  const navigate = useNavigate();
+
   const [product, setProduct] = useState<ProductDTO>();
 
   useEffect(() => {
-    productService.findById(Number(params.productId)).then((response) => {
-      console.log(response.data);
-      setProduct(response.data);
-    });
+    productService
+      .findById(Number(params.productId))
+      .then((response) => {
+        setProduct(response.data);
+      })
+      .catch(() => {
+        navigate("/");
+      });
   }, []);
 
   return (
