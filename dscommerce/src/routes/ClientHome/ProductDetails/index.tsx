8ab0addab80c9f2e45b9135182e3d938d6ad9317
Rafael Santos <rafa.05125@gmail.com>
2024-07-18 23:59:15 -0300

    Adding product to cart

--- dscommerce/src/routes/ClientHome/ProductDetails/index.tsx
+++ dscommerce/src/routes/ClientHome/ProductDetails/index.tsx
@@ -7,6 +7,7 @@ import "./styles.css";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { ProductDTO } from "../../../models/product.ts";
+import * as cartService from "../../../services/cart-service.ts";
 
 export default function ProductDetails() {
   const params = useParams();
@@ -26,13 +27,23 @@ export default function ProductDetails() {
       });
   }, []);
 
+  function handleBuyClick() {
+    if (product) {
+      cartService.addProduct(product);
+      navigate("/cart");
+    }
+  }
+
   return (
     <main>
       <section id="product-details-section" className="dsc-container">
         {product && <ProductDetailsCard product={product} />}
 
         <div className="dsc-btn-page-container">
-          <ButtonPrimary text="Comprar" />
+          <div onClick={handleBuyClick}>
+            <ButtonPrimary text="Comprar" />
+          </div>
+
           <Link to="/">
             <ButtonInverse text="Início" />
           </Link>
